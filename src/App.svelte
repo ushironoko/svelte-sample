cb25c996f2858e22cb3ec5ca93030654dbcd887f
ushironoko <apple19940820@yahoo.co.jp>
2019-04-29 22:30:52 +0900

    fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Buttons from '../components/Button.svelte'
+	import Buttons from './components/Button.svelte'
 
 	let incrementalButtonText = ' + '
 	let decrementalButtonText = ' - '
